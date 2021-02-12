60544f06ebbdf2f62a323e72e0caa54905a99dab
曾艳平 <zengyanping@cengyanpingdeMacBook-Pro.local>
2021-02-12 21:19:41 +0800

    +1

--- demo.py
+++ demo.py
@@ -45,4 +45,4 @@ if __name__ == '__main__':
     with open(test,'r') as a:
         config=yaml.safe_load(a)
     print(config['database'])
-    print(config['database'])
+    print(config['database1'])
